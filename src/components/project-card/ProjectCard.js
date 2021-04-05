f74f176a048fcc8cbafeb53760ddb14227f24341
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-04-05 13:26:40 -0700

    add project images.

--- src/components/project-card/ProjectCard.js
+++ src/components/project-card/ProjectCard.js
@@ -5,9 +5,9 @@ const ProjectCard = (props) => (
     <Card style={{ width: '18rem' }}>
         <Card.Img variant="top" src={props.src} />
         <Card.Body>
-            <Card.Title>{props.Name}</Card.Title>
+            <Card.Title>{props.name}</Card.Title>
             <Card.Text>
-                {props.Description}
+                {props.description}
             </Card.Text>
             <Button variant="primary">Go somewhere</Button>
         </Card.Body>
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,6 +1,14 @@
+import ProjectCard from '../components/project-card/ProjectCard';
+import Gif from '../assets/gifs/Music-Spider.gif';
+import Container from 'react-bootstrap/Container';
+
 const Portfolio = () => {
     return (
-        <h1>Hello from Portfolio</h1>
+        <Container>
+            <h1>Hello from Portfolio</h1>
+            <ProjectCard src={Gif} name='Music Spider' description='A music finding app that allows the user to search up songs by lyrics and discover cool new artists.'/>
+        </Container>
+
     )
 }
 
