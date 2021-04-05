6cc616556c7da6df7ec4001d9221d4f81822b234
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-04-05 09:34:54 -0700

    install react-router, add basic nav and router to all pages in app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import './App.css';
-
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Portfolio from './pages/Portfolio';
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/portfolio">Portfolio</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Switch>
+            <Route path="/portfolio">
+              <Portfolio />
+            </Route>
+            <Route path="/contact">
+              <Contact />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
