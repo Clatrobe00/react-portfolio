5c936f9b1a8acca97694478cfa41643aab6032b8
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-04-06 10:49:26 -0700

    add dropdown to Navbar

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,13 +1,18 @@
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav'
+import { NavDropdown } from 'react-bootstrap';
 
 const NavBar = () => (
-    <Navbar bg="light" expand="lg" style={{ opacity: '50%' }}>
+    <Navbar bg="light" expand="lg" style={{ opacity: '80%', zIndex: 999 }}>
         <Navbar.Brand href="#home">React Portfolio</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
             <Nav.Link href="home">Home</Nav.Link>
+            <NavDropdown title="Portfolio" id="basic-nav-dropdown">
+                <NavDropdown.Item href="portfolio">Projects</NavDropdown.Item>
+                <NavDropdown.Item href="resume">Resume</NavDropdown.Item>
+            </NavDropdown>
             <Nav.Link href="portfolio">Portfolio</Nav.Link>
             <Nav.Link href="contact">Contact</Nav.Link>
             </Nav>
