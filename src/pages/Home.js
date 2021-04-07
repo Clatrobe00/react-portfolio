b165c52fa5eabe09ee7cced93f61e7429c4eec85
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-04-07 12:18:44 -0700

    update profile card with github link

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ import profileImage from '../assets/pictures/Profile.jpg';
 const Home = () => {
     return (
         <Container>
-            <ProjectCard src={profileImage} name='Charles Latrobe' description='Full stack developer specializing in Node and React'/>
+            <ProjectCard src={profileImage} name='Charles Latrobe' description='Full stack developer specializing in Node and React' link='https://github.com/Clatrobe00' children='My Github'/>
         </Container>
     )
 }
