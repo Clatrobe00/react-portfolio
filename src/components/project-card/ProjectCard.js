6d395ea19c658ae356df5922dc8300b1d7ddf906
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-04-07 15:32:41 -0700

    update project-card with conditional rendering

--- src/components/project-card/ProjectCard.js
+++ src/components/project-card/ProjectCard.js
@@ -1,7 +1,25 @@
 import Card from 'react-bootstrap/Card';
 import ProjectButton from '../button/ProjectButton';
 
-const ProjectCard = (props) => (
+const ProjectCard = (props) => {
+    
+    if (props.children && props.link) {
+        return (
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={props.src} />
+                <Card.Body>
+                    <Card.Title>{props.name}</Card.Title>
+                    <Card.Text>
+                        {props.description}
+                    </Card.Text>
+                </Card.Body>
+                <ProjectButton link={props.link}>
+                    {props.children}
+                </ProjectButton>
+            </Card>
+        )
+    } else
+    return (
     <Card style={{ width: '18rem' }}>
         <Card.Img variant="top" src={props.src} />
         <Card.Body>
@@ -10,10 +28,7 @@ const ProjectCard = (props) => (
                 {props.description}
             </Card.Text>
         </Card.Body>
-        <ProjectButton link={props.link}>
-            {props.children}
-        </ProjectButton>
     </Card>
-)
+)}
 
 export default ProjectCard;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,14 @@
-import Container from 'react-bootstrap/Container';
+import { Container, CardColumns } from 'react-bootstrap';
 import ProjectCard from '../components/project-card/ProjectCard'
 import profileImage from '../assets/pictures/Profile.jpg';
 
 const Home = () => {
     return (
         <Container>
-            <ProjectCard src={profileImage} name='Charles Latrobe' description='Full stack developer specializing in Node and React' link='https://github.com/Clatrobe00' children='My Github'/>
+            <CardColumns>
+                <ProjectCard src={profileImage} name='Charles Latrobe' description='Full stack developer specializing in Node and React' link='https://github.com/Clatrobe00' children='My Github'/>
+                <ProjectCard name='Technologies' description='React.js' children={false}/>
+            </CardColumns>
         </Container>
     )
 }
