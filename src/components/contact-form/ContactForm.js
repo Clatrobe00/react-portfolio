dceca8d693806f3d06a57c50caee1ac71e610e37
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-04-07 12:45:48 -0700

    add button to contact form, add handleSubmit function to contact page.

--- src/components/contact-form/ContactForm.js
+++ src/components/contact-form/ContactForm.js
@@ -1,23 +1,33 @@
 import Form from 'react-bootstrap/Form';
+import ProjectButton from '../button/ProjectButton';
 
-const ContactForm = () => (
-    <Form>
-        <Form.Group controlId="formBasicName">
-            <Form.Label>Name</Form.Label>
-            <Form.Control type="name" placeholder="Enter name" />
-        </Form.Group>
-        <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email</Form.Label>
-            <Form.Control type="password" placeholder="E-mail" />
-            <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-            </Form.Text>
-        </Form.Group>
-        <Form.Group controlId="formBasicName">
-            <Form.Label>Message</Form.Label>
-            <Form.Control as="textArea" rows={3} type="name" placeholder="Leave a brief description of you project." />
-        </Form.Group>
-    </Form>
-)
+const ContactForm = () => {
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('submitted!')
+    }
+    return (
+        <Form>
+            <Form.Group controlId="formBasicName">
+                <Form.Label>Name</Form.Label>
+                <Form.Control type="name" placeholder="Enter name" />
+            </Form.Group>
+            <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="password" placeholder="E-mail" />
+                <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+                </Form.Text>
+            </Form.Group>
+            <Form.Group controlId="formBasicName">
+                <Form.Label>Message</Form.Label>
+                <Form.Control as="textArea" rows={3} type="name" placeholder="Leave a brief description of you project." />
+            </Form.Group>
+            <ProjectButton onClick={handleSubmit} children='submit'/>
+        </Form>
+    )
+
+}
 
 export default ContactForm;
