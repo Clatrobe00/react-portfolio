37e3512aea72a279e644f38d7ec59a2c71ce9896
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-04-08 09:35:48 -0700

    style project cards and title card

--- src/components/project-card/ProjectCard.js
+++ src/components/project-card/ProjectCard.js
@@ -1,11 +1,12 @@
 import Card from 'react-bootstrap/Card';
 import ProjectButton from '../button/ProjectButton';
+import './ProjectCard.css';
 
 const ProjectCard = (props) => {
     
     if (props.children && props.link) {
         return (
-            <Card style={{ width: '18rem' }}>
+            <Card className='genCard projectCard'>
                 <Card.Img variant="top" src={props.src} />
                 <Card.Body>
                     <Card.Title>{props.name}</Card.Title>
@@ -13,14 +14,19 @@ const ProjectCard = (props) => {
                         {props.description}
                     </Card.Text>
                 </Card.Body>
+                <p>
                 <ProjectButton link={props.link}>
                     {props.children}
                 </ProjectButton>
+                <ProjectButton link={props.repo} buttonStyle='btn--outline'>
+                    Repo
+                </ProjectButton>
+                </p>
             </Card>
         )
     } else
     return (
-    <Card style={{ width: '18rem' }}>
+    <Card className='genCard titleCard'>
         <Card.Img variant="top" src={props.src} />
         <Card.Body>
             <Card.Title>{props.name}</Card.Title>
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -3,7 +3,7 @@ import musicSpiderGif from '../assets/gifs/Music-Spider.gif';
 import liaBanner from '../assets/pictures/ChineseLanterns.jpg';
 import cyoaBanner from '../assets/pictures/Castaway.jpg';
 import Container from 'react-bootstrap/Container';
-import CardColumns from 'react-bootstrap/CardColumns';
+import CardDeck from 'react-bootstrap/CardDeck';
 import comingSoon from '../assets/pictures/ComingSoon.jpg';
 import learningApp from '../assets/pictures/learningApp.jpg';
 import burgerLogger from '../assets/pictures/burgerLogger.jpg';
@@ -11,15 +11,17 @@ import burgerLogger from '../assets/pictures/burgerLogger.jpg';
 const Portfolio = () => {
     return (
         <Container>
-            <CardColumns style={{ display: 'flex', flexWrap: 'wrap' }}>
+            <CardDeck>
                 <ProjectCard src={musicSpiderGif} name='Music Spider' description='A music finding app that allows the user to search up songs by lyrics and discover cool new artists.' link='https://inesr19.github.io/Project-1/' children='Find some music'/>
                 <ProjectCard src={liaBanner} name='Lia Liu Chinese' description='A website for Lia Liu Chinese built with React.' link='https://lia-liu-chinese.herokuapp.com/' children='Learn Chinese'/>
-                <ProjectCard src={cyoaBanner} name='Castaway: Flight of the Phoenix' description='A choose-your-own-adventure style game that challenges the user to escape an alien planet.' link='https://cyoa-rpg.herokuapp.com/' children='Try your luck'/>
+                <ProjectCard src={cyoaBanner} name='Castaway: Flight of the Phoenix' description='A choose-your-own-adventure style game that challenges the user to escape an alien planet.'
+                link='https://cyoa-rpg.herokuapp.com/' children='Try your luck'/>
+            </CardDeck>
+            <CardDeck>    
                 <ProjectCard src={comingSoon} name='Coming Soon' description='My latest incredible project is coming soon.'/>
                 <ProjectCard src={learningApp} name='Language Learning App' description='A mobile-first language learning app for children.' link='https://github.com/kurtp23/language-app' children='Expand your mind'/>
                 <ProjectCard src={burgerLogger} name='The Burger Logger' description='A full-stack app built with Node, Express, SQL, and HandleBars.' link='https://github.com/Clatrobe00/Burger-Logger' children='Grab a burger'/>
-                
-            </CardColumns>    
+            </CardDeck>    
         </Container>
     )
 }
