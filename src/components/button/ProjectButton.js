0f9de25cff8d292f549e6b218b33183b84701ec3
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-04-07 20:26:57 -0700

    style button

--- src/components/button/ProjectButton.js
+++ src/components/button/ProjectButton.js
@@ -4,15 +4,15 @@ import './Button.css'
 
 const STYLES = ['btn--primary', 'btn--outline'];
 
-const SIZES = ['btn--medium', 'btn--large'];
+const SIZES = ['btn--small', 'btn--large'];
 
 const ProjectButton = (props) => {
-    const checkButtonStyle = STYLES.includes(props.buttonStyle) ? props.buttonStyle : STYLES[1];
+    const checkButtonStyle = STYLES.includes(props.buttonStyle) ? props.buttonStyle : STYLES[0];
 
     const checkButtonSize = SIZES.includes(props.buttonSize) ? props.buttonSize : SIZES[0];
 
     return (
-        <a href={props.link} rel='noreferrer' target='_blank' className='btnMobile'>
+        <a href={props.link} rel='noreferrer' buttonSize={props.buttonSize} buttonStyle={props.buttonStyle} target='_blank' className='btnMobile'>
             <button className={`btn ${checkButtonStyle} ${checkButtonSize}`} onClick={props.onClick} type={props.type} >
                 {props.children}
             </button>
