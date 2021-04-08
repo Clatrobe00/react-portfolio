0a629f1e830fb0a0411c537ffeb1c7f6b6f75e79
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-04-08 10:30:35 -0700

    style resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import './Resume.css';
 import { Document, Page } from 'react-pdf';
 import { pdfjs } from 'react-pdf';
 import resume from '../assets/pdf/LatrobeEDResumePDF.pdf';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
+
 const Resume = () => (
+    <div className='resumeContainer'>
         <Document file={resume}>
             <Page pageNumber={1}/>
             <br />
             <Page pageNumber={2}/>
         </Document>
+    </div>
+
 )
     
 
