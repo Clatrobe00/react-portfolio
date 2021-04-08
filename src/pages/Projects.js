2864f9d3078909d9ad7ed91db12d572393ea91eb
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-04-08 09:43:32 -0700

    add repo links to cards

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -12,15 +12,15 @@ const Portfolio = () => {
     return (
         <Container>
             <CardDeck>
-                <ProjectCard src={musicSpiderGif} name='Music Spider' description='A music finding app that allows the user to search up songs by lyrics and discover cool new artists.' link='https://inesr19.github.io/Project-1/' children='Find some music'/>
-                <ProjectCard src={liaBanner} name='Lia Liu Chinese' description='A website for Lia Liu Chinese built with React.' link='https://lia-liu-chinese.herokuapp.com/' children='Learn Chinese'/>
+                <ProjectCard src={musicSpiderGif} name='Music Spider' description='A music finding app that allows the user to search up songs by lyrics and discover cool new artists.' link='https://inesr19.github.io/Project-1/' repo='https://github.com/Clatrobe00/Music-Spider' children='Find some music'/>
+                <ProjectCard src={liaBanner} name='Lia Liu Chinese' description='A website for Lia Liu Chinese built with React.' link='https://lia-liu-chinese.herokuapp.com/' repo='https://github.com/Clatrobe00/liachinese' children='Learn Chinese'/>
                 <ProjectCard src={cyoaBanner} name='Castaway: Flight of the Phoenix' description='A choose-your-own-adventure style game that challenges the user to escape an alien planet.'
-                link='https://cyoa-rpg.herokuapp.com/' children='Try your luck'/>
+                link='https://cyoa-rpg.herokuapp.com/' repo='https://github.com/Clatrobe00/CYOA' children='Try your luck'/>
             </CardDeck>
             <CardDeck>    
                 <ProjectCard src={comingSoon} name='Coming Soon' description='My latest incredible project is coming soon.'/>
-                <ProjectCard src={learningApp} name='Language Learning App' description='A mobile-first language learning app for children.' link='https://github.com/kurtp23/language-app' children='Expand your mind'/>
-                <ProjectCard src={burgerLogger} name='The Burger Logger' description='A full-stack app built with Node, Express, SQL, and HandleBars.' link='https://github.com/Clatrobe00/Burger-Logger' children='Grab a burger'/>
+                <ProjectCard src={learningApp} name='Language Learning App' description='A mobile-first language learning app for children.' repo='https://github.com/kurtp23/language-app' link='https://github.com/kurtp23/language-app' children='Expand your mind'/>
+                <ProjectCard src={burgerLogger} name='The Burger Logger' description='A full-stack app built with Node, Express, SQL, and HandleBars.' repo='https://github.com/Clatrobe00/Burger-Logger' link='https://burger-logger-clb.herokuapp.com/' children='Grab a burger'/>
             </CardDeck>    
         </Container>
     )
