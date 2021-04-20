1c44b71385f5dfb8282c2f4e3870adab34386a10
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-04-20 14:57:39 -0700

    add card flip animation

--- src/components/project-card/ProjectCard.js
+++ src/components/project-card/ProjectCard.js
@@ -1,29 +1,63 @@
 import Card from 'react-bootstrap/Card';
 import ProjectButton from '../button/ProjectButton';
 import './ProjectCard.css';
+import { useSpring, animated as a } from 'react-spring'
+import React, { useState } from 'react'
 
 const ProjectCard = (props) => {
+
+    const [flipped, set] = useState(false)
+    const { transform, opacity } = useSpring({
+      opacity: flipped ? 1 : 0,
+      transform: `perspective(300px) rotateX(${flipped ? 180 : 0}deg)`,
+      config: { mass: 5, tension: 600, friction: 80 }
+    })
     
     if (props.children && props.link) {
         return (
-            <Card className='genCard projectCard'>
-                <Card.Img variant="top" src={props.src} />
-                <Card.Body>
-                    <Card.Title>{props.name}</Card.Title>
-                    <Card.Text>
-                        {props.description}
-                    </Card.Text>
-                </Card.Body>
-                <p>
-                <ProjectButton link={props.link}>
-                    {props.children}
-                </ProjectButton>
-                <ProjectButton link={props.repo} buttonStyle='btn--outline'>
-                    Repo
-                </ProjectButton>
-                </p>
-            </Card>
-        )
+            <div className='templateContainer'>
+                <div onClick={() => set(state => !state)}>
+                    <a.div class="c back" style={{ opacity: opacity.interpolate(o => 1 - o), transform }}>
+                        <Card className='genCard projectCard'>
+                            <Card.Img variant="top" src={props.src} />
+                            <Card.Body>
+                                <Card.Title>{props.name}</Card.Title>
+                                <Card.Text>
+                                    {props.description}
+                                </Card.Text>
+                            </Card.Body>
+                            <p>
+                            <ProjectButton link={props.link}>
+                                {props.children}
+                            </ProjectButton>
+                            <ProjectButton link={props.repo} buttonStyle='btn--outline'>
+                                Repo
+                            </ProjectButton>
+                            </p>
+                        </Card>
+                    </a.div>
+                    <a.div class="c front" style={{ opacity, transform: transform.interpolate(t => `${t} rotateX(180deg)`) }}>
+                        <Card className='genCard projectCard'>
+                            <Card.Img variant="top" src={props.src} />
+                            <Card.Body>
+                                <Card.Title>{props.name}</Card.Title>
+                                <Card.Text>
+                                    {props.description}
+                                </Card.Text>
+                            </Card.Body>
+                            <p>
+                            <ProjectButton link={props.link}>
+                                {props.children}
+                            </ProjectButton>
+                            <ProjectButton link={props.repo} buttonStyle='btn--outline'>
+                                Repo
+                            </ProjectButton>
+                            </p>
+                        </Card>
+                    </a.div>
+                </div>
+            </div>    
+                    )
     } else
     return (
     <Card className='genCard titleCard'>
