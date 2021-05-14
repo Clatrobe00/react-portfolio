279d13c60aa14ac14497078c9c377bf90b24b1ab
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-05-14 13:30:30 -0700

    add new jumbo component to homepage.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,17 +3,12 @@ import ProjectCard from '../components/project-card/ProjectCard';
 import profileImage from '../assets/pictures/Profile.jpg';
 import TechList from '../components/tech-list/TechList';
 import Bio from '../components/bio/Bio';
+import HomeJumbo from '../components/home-jumbo/HomeJumbo';
 
 const Home = () => {
     return (
         <Container>
-            <CardColumns>
-                <ProjectCard src={profileImage} name='Charles Latrobe' description='Full stack developer specializing in Node.js and React' link='https://github.com/Clatrobe00' children='My Github'/>
-                <ProjectCard name='Technologies' description={false}>
-                    <TechList />
-                </ProjectCard>
-                {/* <Bio /> */}
-            </CardColumns>
+            <HomeJumbo />
         </Container>
     )
 }
