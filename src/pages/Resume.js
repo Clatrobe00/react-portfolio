2221f4075a53841d59405a1c685c8f1de0466ab8
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-05-17 17:45:42 -0700

    update resume viewer with new node package. add improved viewing in mobile

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,35 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { Worker } from '@react-pdf-viewer/core';
+import { Viewer } from '@react-pdf-viewer/core';
+import '@react-pdf-viewer/core/lib/styles/index.css';
 import './Resume.css';
-import { Document, Page } from 'react-pdf';
 import { pdfjs } from 'react-pdf';
 import resume from '../assets/pdf/LatrobeWDResume.pdf';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 
 const Resume = () => {
-    const getDimensions = () => {
-        const {innerWidth: width, innerHeight: height} = window
-        return {width, height}
-    }
-    const [dimensions, setDimensions] = useState(getDimensions());
-    
-    useEffect (() => {
-        const handleResize = () => {
-            console.log('hello')
-            setDimensions(getDimensions())
-        }
-        window.addEventListener('resize', handleResize);
-        return () => window.removeEventListener('resize', handleResize)
-
-    }, []) 
 
     return (
     <div className='resumeContainer'>
-        <Document file={resume}>
-            <Page width={dimensions.width < 600 ? '300' : '600'} pageNumber={1}/>
-            <br />
-            <Page width={dimensions.width < 600 ? '300' : '600'} pageNumber={2}/>
-        </Document>
+        <Worker className='worker-pdf' workerUrl="https://unpkg.com/pdfjs-dist@2.6.347/build/pdf.worker.min.js">
+            <Viewer fileUrl={resume} />
+        </Worker>
     </div>
 
 )}
