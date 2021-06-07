45a99f07b4bb4f31270fa1d65c5bdc293498b1d0
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-06-06 17:48:58 -0700

    update footer with new info

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,6 +2,7 @@ import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/Container';
 import './Footer.css';
 import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { SiNetlify } from 'react-icons/si';
 
 const Footer = () => (
     <Jumbotron className='Footer' fluid>
@@ -13,6 +14,7 @@ const Footer = () => (
             <a href="https://www.linkedin.com/in/charles-latrobe-bateman-6b748a1b6/" rel="noreferrer" target="_blank" className="footer-links">
                 <FaLinkedin style={{ fontSize: '2rem', marginLeft: '5px', marginRight: '5px' }}/>
             </a>
+            <p>Hosted with Netlify <SiNetlify /></p>
         </Container>
     </Jumbotron>
 )
