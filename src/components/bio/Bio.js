14d62de6c7546385b6728ca0f87d64e3ba3b4f0d
Charles Latrobe <Charlie.bateman117@gmail.com>
2021-06-15 12:43:10 -0700

    update bio

--- src/components/bio/Bio.js
+++ src/components/bio/Bio.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Bio = () => (
     <div>
-        <p>I am a Full-Stack web developer specializing in React and Node applications. My passion in programming is for Algorithm design and working with other developers to make intuitive, engaging applications- finding the solution to a complex, multifaceted puzzle is, for me, one of the best feelings out there.</p>
+        <p>I am a Full-Stack web developer specializing in React and Node applications. My passion in programming is for Algorithm design and working with other developers to make intuitive, engaging applications.</p>
         <br/>
     </div>
 )
